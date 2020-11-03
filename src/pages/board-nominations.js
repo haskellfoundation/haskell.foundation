7ab7d95095e08ac32d7d0944f181dabf1055d749
Rebecca Skinner <rebecca@rebeccaskinner.net>
2020-11-02 22:16:27 -0600

    add board nominations text

---
+++ src/pages/board-nominations.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Grid from '@material-ui/core/Grid'
+import Layout from '../components/Layout'
+import Page from '../components/Page'
+import BoardNominationsPageQuery from '../queries/BoardNominationsPageQuery'
+import Markdown from '@input-output-hk/front-end-core-components/components/Markdown'
+
+export default () => (
+  <BoardNominationsPageQuery
+    render={(content) => (
+      <Layout>
+        <Page title={content.about_page_title}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <Markdown source={content.about_page_content} />
+            </Grid>
+          </Grid>
+        </Page>
+      </Layout>
+    )}
+  />
+)
